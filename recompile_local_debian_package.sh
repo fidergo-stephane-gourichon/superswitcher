71499d497b2c8a2258fc90f65cf3ce9091c877e0
Stéphane Gourichon <stephane@gourichon.org>
2020-02-11 13:52:29 +0100

    More recent debian package build script.

--- recompile_local_debian_package.sh
+++ recompile_local_debian_package.sh
@@ -1,29 +1,71 @@
 #!/bin/bash
 
-TMPDIR=$( mktemp -d ) && echo "TMPDIR is $TMPDIR"
+cat <<EOF
+========================================================================
+One-step Debian package generation script.
+========================================================================
 
-set -eu
+This script takes a git-version-controlled tree hierarchy containing
+a proper debian/ directory for package generation, does a temporary
+clone and builds debian packages from it.
+
+Added value :
+* automatically generated a proper *.orig.tar.gz as required by debuild.
+* make sure the build it not polluted by any local non-commited files
+* keep the original tree hierarchy clean
+
+This script should be generic enough to be used in other programe.  It
+might be confused by stray version names with strange or worse, evil,
+characters, but if you name your package "little bobby tables" you
+deserve to to all this by hand.
+
+Written by Stéphane Gourichon <stephane_dpkg@gourichon.org>
+
+========================================================================
+
+Let's go!
+
+
+EOF
+
+set -euo pipefail
+
+cd "$(dirname "$(readlink -f "$0")" )"
+
+dpkg-checkbuilddeps
+echo -e "* dpkg-checkbuilddeps\tPASSED"
 
 PKGDIR="$PWD"
 
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TMPDIR=$( mktemp -d ) && echo "* Will work in temp dir $TMPDIR"
 
-echo "On branch $CURRENT_BRANCH"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD) && echo "* Current git branch $CURRENT_BRANCH"
 
-if output=$(git status --porcelain) && [ -z "$output" ]; then
-    echo "Working directory clean"
-else 
-    echo >&2 "Uncommitted changes. ABORT."
-    git status
-    echo >&2 "Uncommitted changes. ABORT."
+NAMEFORTAR="$( head -n 1 debian/changelog | sed -n 's/^\([^ ]*\)* (\([^-]*\)-[0-9]*).*$/\1_\2/p' )"
+DIRNAMEFORDEB=${NAMEFORTAR//_/-}
+
+if [[ -z "$NAMEFORTAR" ]]
+then
+    echo >&2 "Cannot figure out tar archive name from first line of debian/changelog. Aborting"
+    head -n 1 debian/changelog
     exit 1
 fi
 
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+    echo "Working directory clean"
+else
+    echo >&2 "WARNING: uncommitted changes. Consider aborting."
+    git status
+    echo >&2 "WARNING: uncommitted changes. Consider aborting."
+    echo >&2 "Waiting for 10 second."
+    sleep 10
+fi
 
 cd "$TMPDIR"
-git clone "$PKGDIR"
-tar zcvf superswitcher_0.9.orig.tar.gz superswitcher
-cd superswitcher
+git clone "$PKGDIR" "${DIRNAMEFORDEB}"
+
+tar zcvf ${NAMEFORTAR}.orig.tar.gz "${DIRNAMEFORDEB}"
+cd "${DIRNAMEFORDEB}"
 
 dpkg-checkbuilddeps
 debuild -us -uc
